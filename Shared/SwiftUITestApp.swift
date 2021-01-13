603bda62f613d1786cf22bda77fc0e5a9f0752c9
CHOITAEHYUNG <cth400@naver.com>
2021-01-13 16:18:35 +0100

    Initial Commit

---
+++ Shared/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  Shared
+//
+//  Created by TAEHYUNG CHOI on 13/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
